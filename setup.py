5e563585d6a7c25a1e4a5b62e59118206b375a57
cc215 <chen.chen15@imperial.ac.uk>
2021-07-28 18:23:55 +0100

    update

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="advchain",
-    version="0.16",
+    version="0.18",
     author="Chen (Cherise) Chen",
     author_email="work.cherise@gmail.com",
     description="adversarial data augmentation with chained  transformations",
@@ -20,7 +20,7 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    package_dir={"": "advchain"},
-    packages=setuptools.find_packages(where="advchain"),
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
     python_requires=">=3.6",
 )
